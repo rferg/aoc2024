abdaf499293d0b6e430224236b074f93602806c9
rferg <rmferguson77@gmail.com>
2024-12-13 17:02:12 -0500

    (WIP) checkpoint: unfinished day 9 part 2

--- 09/solution.rb
+++ 09/solution.rb
@@ -10,7 +10,7 @@ class Solution
     line.chars.each_with_index do |c, i|
       id = i.even? ? i / 2 : nil
       len = c.to_i
-      len.times { buffer << id }
+      buffer += Array.new(len, id)
       files_length += len if i.even?
     end
 
@@ -25,6 +25,48 @@ class Solution
   end
   
   def part_two(lines)
-    # TODO
+    line = lines.first
+    buffer = []
+    file_length_ends = []
+
+    line.chars.each_with_index do |c, i|
+      id = i.even? ? i / 2 : nil
+      len = c.to_i
+      buffer += Array.new(len, id)
+      file_length_ends[id] = [len, buffer.length] unless id.nil?
+    end    
+
+    file_length_ends.to_enum.with_index.reverse_each do |len_end, id|
+      len, end_pos = len_end
+      starts = end_pos - len
+      current_pos = 0
+      while((nil_idx, non_nil_idx = next_free_chunk(current_pos, buffer)))
+        puts "starts #{starts}, nil_idx: #{nil_idx}, non_nil_idx: #{non_nil_idx}, current_pos: #{current_pos}"
+        break if starts < nil_idx
+
+        if (nil_idx - non_nil_idx) >= len
+          buffer[first_nil_index...(first_nil_index + len)] = Array.new(len, id)
+          buffer[(end_pos - len)...end_pos] = Array.new(len, nil)
+          break
+        end
+
+        current_pos = non_nil_idx
+      end
+    end
+
+    puts buffer.map { |x| x.nil? ? "." : x.to_s }.join
+    buffer.length.times.sum { |i| buffer[i].nil? ? 0 : buffer[i] * i }
+  end
+
+  def next_free_chunk(start, buffer)
+    nil_idx = buffer[start..].find_index(nil)
+    return nil if nil_idx.nil?
+
+    non_nil_idx = nil_idx
+    while (buffer[non_nil_idx].nil?)
+      non_nil_idx += 1
+    end
+    
+    [nil_idx, non_nil_idx]
   end
 end
